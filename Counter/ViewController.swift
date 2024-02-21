463ff1a936a32cb92743b47afd0cec479a201988
DarkRaven <aleksandrdugaev@gmail.com>
2024-02-21 21:16:48 +0300

    Изменил переменных и удалил сохранение в память устройства

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -14,44 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var reducingCounterValueButton: UIButton!
     @IBOutlet weak var valueOfCounterLabel: UILabel!
     @IBOutlet weak var historyOfCounterChangesTextView: UITextView!
-    // Самый простой способ — использовать UserDefaults.standard, без всяких оберток:
-    let defaults = UserDefaults.standard
     var counter: Int = 0
     var changeHistoryArray: [String] = []
-    var tempString = ""
-    
-    //var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Достаем значение changeHistoryArray из UserDefaults
-        changeHistoryArray = defaults.stringArray(forKey: "changeHistory") ?? ["История изменений"]
+        valueOfCounterLabel.text = "0"
         updateCounterChangeHistory()
     }
     
     func updateCounterChangeHistory() {
-        tempString = ""
-        for text in changeHistoryArray {
-            tempString += text + "\n"
+        var outputStringInTextViev = "История изменений: \n"
+        for change in changeHistoryArray {
+            outputStringInTextViev += change + "\n"
         }
-        historyOfCounterChangesTextView.text = "\(tempString)"
-        // Сохраняем значение changeHistoryArray в UserDefaults
-        defaults.set(changeHistoryArray, forKey: "changeHistory")
+        historyOfCounterChangesTextView.text = "\(outputStringInTextViev)"
+
     }
     
     func formatedDateTimeMoment() -> String {
-        let dateNow = Date()
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "[dd.MM.yy HH:mm:ss]"
-
-        // let weekday = dateFormatter.string(from: dateNow)
-        return dateFormatter.string(from: dateNow)
+        return dateFormatter.string(from: Date())
     }
     
     @IBAction func increasingCounterValueTouchUpInside(_ sender: Any) {
         counter += 1
         valueOfCounterLabel.text = "Значение счётчика: \(counter)"
-        //changeHistoryArray.append("Значение счётчика увеличено")
         changeHistoryArray.insert("\(formatedDateTimeMoment()): значение изменено на +1", at: 0)
         updateCounterChangeHistory()
         
@@ -75,19 +64,5 @@ class ViewController: UIViewController {
             updateCounterChangeHistory()
         }
     }
-    
-    @IBAction func deletingHistoryOfCounterTouchUpInside(_ sender: Any) {
-        changeHistoryArray = ["История изменений"]
-        updateCounterChangeHistory()
-        //
-        UserDefaults.standard.removeObject(forKey: "changeHistory")
-        UserDefaults.standard.synchronize()
-    }
-    
-    // удаление
-    // UserDefaults.standard.removeObject(forKey: "userAge")
-    // синхронизация
-    // UserDefaults.standard.synchronize()
-    
 }
 
