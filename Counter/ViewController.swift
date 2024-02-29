5fe981a52f2a353cf501e56776d0466370e30fde
DarkRaven <aleksandrdugaev@gmail.com>
2024-02-29 19:51:51 +0300

    изменил модификатор доступа для переменных и функций и поправил очепятки

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,13 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var increasingCounterValueButton: UIButton!
-    @IBOutlet weak var resettingCounterValueButton: UIButton!
-    @IBOutlet weak var reducingCounterValueButton: UIButton!
-    @IBOutlet weak var valueOfCounterLabel: UILabel!
-    @IBOutlet weak var historyOfCounterChangesTextView: UITextView!
-    var counter: Int = 0
-    var changeHistoryArray: [String] = []
+    @IBOutlet private weak var increasingCounterValueButton: UIButton!
+    @IBOutlet private weak var resettingCounterValueButton: UIButton!
+    @IBOutlet private weak var reducingCounterValueButton: UIButton!
+    @IBOutlet private weak var valueOfCounterLabel: UILabel!
+    @IBOutlet private weak var historyOfCounterChangesTextView: UITextView!
+    private var counter: Int = 0
+    private var changeHistoryArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,44 +23,44 @@ class ViewController: UIViewController {
         updateCounterChangeHistory()
     }
     
-    func updateCounterChangeHistory() {
-        var outputStringInTextViev = "История изменений: \n"
+    private func updateCounterChangeHistory() {
+        var outputStringInTextView = "История изменений: \n"
         for change in changeHistoryArray {
-            outputStringInTextViev += change + "\n"
+            outputStringInTextView += change + "\n"
         }
-        historyOfCounterChangesTextView.text = "\(outputStringInTextViev)"
+        historyOfCounterChangesTextView.text = "\(outputStringInTextView)"
 
     }
     
-    func formatedDateTimeMoment() -> String {
+    private func formattedDateTimeMoment() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "[dd.MM.yy HH:mm:ss]"
         return dateFormatter.string(from: Date())
     }
     
-    @IBAction func increasingCounterValueTouchUpInside(_ sender: Any) {
+    @IBAction private func increasingCounterValueTouchUpInside(_ sender: Any) {
         counter += 1
         valueOfCounterLabel.text = "Значение счётчика: \(counter)"
-        changeHistoryArray.insert("\(formatedDateTimeMoment()): значение изменено на +1", at: 0)
+        changeHistoryArray.insert("\(formattedDateTimeMoment()): значение изменено на +1", at: 0)
         updateCounterChangeHistory()
         
     }
     
-    @IBAction func resettingCounterValueTouchUpInside(_ sender: Any) {
+    @IBAction private func resettingCounterValueTouchUpInside(_ sender: Any) {
         counter = 0
         valueOfCounterLabel.text = "Значение счётчика: \(counter)"
-        changeHistoryArray.insert("\(formatedDateTimeMoment()): значение сброшено", at: 0)
+        changeHistoryArray.insert("\(formattedDateTimeMoment()): значение сброшено", at: 0)
         updateCounterChangeHistory()
     }
     
-    @IBAction func reducingCounterValueTouchUpInside(_ sender: Any) {
+    @IBAction private func reducingCounterValueTouchUpInside(_ sender: Any) {
         if counter == 0 {
-            changeHistoryArray.insert("[\(formatedDateTimeMoment())]: попытка уменьшить значение счетчика ниже 0", at: 0)
+            changeHistoryArray.insert("[\(formattedDateTimeMoment())]: попытка уменьшить значение счетчика ниже 0", at: 0)
             updateCounterChangeHistory()
         } else {
             counter -= 1
             valueOfCounterLabel.text = "Значение счётчика: \(counter)"
-            changeHistoryArray.insert("\(formatedDateTimeMoment()): значение изменено на -1", at: 0)
+            changeHistoryArray.insert("\(formattedDateTimeMoment()): значение изменено на -1", at: 0)
             updateCounterChangeHistory()
         }
     }
